82bc41965bb2a473c7d84a08a2ceddd73df0669b
SHU0744 <miyavi.senshu@gmail.com>
2023-12-30 19:59:19 +0900

    テスト

--- libs/client.js
+++ libs/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN,
-  apiKey: process.env.API_KEY,
+  serviceDomain: "q07b16gzht",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
 });
